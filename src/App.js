d8567539b93856ce811859633fcd6718c025cc9a
NAMAN AVASTHI <naman.avasthi@gmail.com>
2017-06-23 15:11:14 -0700

    Adding Bootstrap CDNs
    added in src/App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+      <!-- Latest compiled and minified JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+      
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
