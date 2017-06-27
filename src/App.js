201a2c46b5b68eb76b9aa23c4dfe190c23a47293
NAMAN AVASTHI <naman.avasthi@gmail.com>
2017-06-27 09:06:14 -0700

    home page changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './main_large.PNG';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,24 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="header-wrapper">
+            <span className="header-span">Home</span>
+            <span className="header-span">About</span>
+            <span className="header-span">Projects</span>
+            <span className="header-span">Contact</span>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="home-wrapper">
+          <div className="home-image">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="home-content">
+            <span className="home-heading">Naman Avasthi</span>
+            <br />
+            <span className="home-content-text">Here is a page about an awesome guy named, Naman Avasthi who is currently studying his Masters in Computer Science at University of Southern California. </span>
+          </div>
+        </div>
+        
       </div>
     );
   }
